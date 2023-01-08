0c0a9c8eab42df513de6039ad5223d9bbe61c7d5
Sergio Peña <sergiop79@gmail.com>
2023-01-08 11:24:49 +0100

    Section 3 completed

---
+++ src/admin/app.tsx
@@ -0,0 +1,21 @@
+export default {
+  config: {
+    locales: [
+      'es',
+      'eu'
+    ],
+    // eu: {
+    //   'Auth.form.email.label': 'Posta elektronikoa',
+    //   Users: 'Erabiltzaileak',
+    //   City: 'Hiria',
+    //   // Customize the label of the Content Manager table.
+    //   Id: 'ID euskaraz',
+    // },
+    // languageNativeNames: {
+    //   eu: 'Euskara',
+    // },
+  },
+  bootstrap(app) {
+    console.log(app);
+  },
+};
