a9abd04da1b1fb8b784d97d4ed0da5dfd1dabd3e
Sergio Peña <sergiop79@gmail.com>
2023-01-15 12:28:43 +0100

    Chapter 68

--- src/api/post/services/post.ts
+++ src/api/post/services/post.ts
@@ -4,4 +4,35 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::post.post');
+export default factories.createCoreService('api::post.post', ({ strapi }) => ({
+    // Method 1: Creating an entirely custom service
+    async exampleService(...args) {
+        console.log('Service was called', args);
+        
+        let response = { okay: true }
+
+        if (response.okay === false) {
+            return { response, error: true }
+        }
+
+        return response
+    },
+
+    // Method 2: Wrapping a core service (leaves core logic in place)
+    async find(...args) {
+        // Calling the default core controller
+        const { results, pagination } = await super.find(...args);
+
+        // some custom logic
+        results.forEach(result => {
+            result.counter = 1;
+        });
+
+        return { results, pagination };
+    },
+
+    // Method 3: Replacing a core service
+    async findOne(entityId, params = {}) {
+        return strapi.entityService.findOne('api::post.post', entityId, this.getFetchParams(params));
+    }
+}));
