db21da3cb65c27d3285e8034800e5e4fe92800b6
Sergio Peña <sergiop79@gmail.com>
2023-01-17 18:58:52 +0100

    Chapter 71, solution 2 (incomplete)

--- src/api/post/controllers/post.ts
+++ src/api/post/controllers/post.ts
@@ -15,15 +15,37 @@ export default factories.createCoreController('api::post.post', ({ strapi }) => 
     }
   },
 
-  // Method 2: Wrapping a core action (leaves core logic in place)
+  // Solution 1:  fetched all posts and filtered them afterwards
+  // async find(ctx) {
+  //   // fetch all the posts
+  //   const { data, meta } = await super.find(ctx);
+
+  //   if (ctx.state.user) return { data, meta }
+
+  //   const filteredData = data.filter((post) => !post.attributes.premium);
+  //   return { data: filteredData, meta }
+  // },
+
+  // Solution 2: rewrite the action to fetch only needed posts
   async find(ctx) {
-    // fetch all the posts
-    const { data, meta } = await super.find(ctx);
+    // if the request is authenticated
+    // const isRequestingNonPremium = ctx.query.filters && ctx.query.filters.premium == false;
+    console.log(ctx.params);
+    
+    if (ctx.state.user) return await super.find(ctx);
 
-    if (ctx.state.user) return { data, meta }
+    // if the request is public ...
+    // let's  call the underlying service with an additional filter: premium == false
+    // /posts?filters[premium]=false
+    const filteredPosts = await strapi.service('api::post.post').find({
+      filters: {
+        premium: false
+      }
+    })
 
-    const filteredData = data.filter((post) => !post.attributes.premium);
-    return { data: filteredData, meta }
+    const sanitizedPosts = await this.sanitizeOutput(filteredPosts, ctx);
+
+    return this.transformResponse(sanitizedPosts);
   },
 
   // Method 3: Replacing a core action
