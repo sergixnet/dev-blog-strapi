5d1ba17f553f6111ef83b709e07abf617fda1c70
Sergio Peña <sergiop79@gmail.com>
2023-01-22 10:17:43 +0100

    Chapter 79

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export default {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register(/*{ strapi }*/) { },
 
   /**
    * An asynchronous bootstrap function that runs before
@@ -14,5 +14,30 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap({ strapi }) {
+    // we listen to lifecycle events
+    strapi.db.lifecycles.subscribe({
+      models: ['admin::user'], // only listen to events for this model
+      afterCreate: async ({ result }) => {
+        // create an Author instance from the fields of the Admin User
+        // that has just been created
+
+        // Extract the fields from newly created Admin user
+        const { id, firstname, lastname, email, username, createdAt, updatedAt } = result;
+
+        await strapi.service('api::author.author').create({
+          data: {
+            firstname,
+            lastname,
+            email,
+            username,
+            createdAt,
+            updatedAt,
+            admin_user: [id]
+          }
+        })
+      }
+    })
+
+  },
 };
