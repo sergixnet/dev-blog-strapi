9e70493e3679a89bb412c17ccafec63036ef402c
Sergio Peña <sergiop79@gmail.com>
2023-02-02 17:35:53 +0100

    Chapter 108

--- src/plugins/github-projects/admin/src/components/Repo.tsx
+++ src/plugins/github-projects/admin/src/components/Repo.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
+  Alert,
   BaseCheckbox,
   Box,
+  Loader,
   Table,
   Thead,
   Tbody,
@@ -10,10 +12,50 @@ import {
   Th,
   Typography,
 } from "@strapi/design-system";
+import axios from "../utils/axiosInstance";
 
 const COL_COUNT = 5;
 
 const Repo = () => {
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({});
+
+  useEffect(() => {
+    setLoading(true);
+
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get("/github-projects/repos");
+        setRepos(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (Object.keys(error).length) {
+    console.log(error);
+
+    return (
+      <Alert
+        closeLabel="Close alert"
+        title="Error fetching repoitories"
+        variant="danger"
+      >
+        {error.toString()}
+      </Alert>
+    );
+  }
+
+  if (loading) return <Loader />;
+
+  console.log(repos);
+
   return (
     <Box padding={8} background="neutral100">
       <Table colCount={COL_COUNT} rowCount={6}>
