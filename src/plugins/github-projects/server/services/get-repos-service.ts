474ee6385d2053be50e83f08a0c4db41347e6e39
Sergio Peña <sergiop79@gmail.com>
2023-02-02 13:20:12 +0100

    Chapter 101

--- src/plugins/github-projects/server/services/get-repos-service.ts
+++ src/plugins/github-projects/server/services/get-repos-service.ts
@@ -1,5 +1,6 @@
-import { request } from '@octokit/request';
-import { Strapi } from '@strapi/strapi';
+import { request } from "@octokit/request";
+import { Strapi } from "@strapi/strapi";
+import axios from "axios";
 
 export default ({ strapi }: { strapi: Strapi }) => ({
   async getPublicRepos() {
@@ -10,6 +11,37 @@ export default ({ strapi }: { strapi: Strapi }) => ({
       type: "public",
     });
 
-    return result;
+    // id, name, shortDecription, url, longDescription
+    // https://raw.githubusercontent.com/sergixnet/dev-blog-strapi/main/README.md
+
+    return Promise.all(
+      result.data.map(async (item) => {
+        const { id, name, description, html_url, owner, default_branch } = item;
+        const readmeUrl = `https://raw.githubusercontent.com/${owner.login}/${name}/${default_branch}/README.md`;
+        let longDescription = "";
+
+        try {
+          const response = await axios.get(readmeUrl);
+          console.log({response});
+
+          if (response.status === 200) {
+            longDescription = response.data;
+          }
+
+        } catch (err) {
+          console.log(err);
+        }
+
+        return {
+          id,
+          name,
+          shortDescription: description,
+          url: html_url,
+          longDescription,
+        };
+      })
+    ).catch((error) => {
+      console.log(error);
+    });
   },
 });
